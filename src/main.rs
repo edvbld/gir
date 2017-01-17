1561c5533afd7a552379eef3e08c9ddd088aee60
Erik Helin <e@dvbld.com>
2017-01-17 20:34:42 +0100

    refactored and moved to pure Rust deps

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,35 @@
-extern crate ring;
-extern crate flate2;
-extern crate rustc_serialize;
+extern crate sha1;
+extern crate deflate;
 
-use ring::digest;
-use flate2::Compression;
-use flate2::write::ZlibEncoder;
-use std::io::Write;
 use std::fs;
 use std::fs::File;
-use rustc_serialize::hex::ToHex;
+use std::io::Write;
+
+fn sha1<'a>(s: &'a String) -> String {
+    let mut m = sha1::Sha1::new();
+    m.update(s.as_bytes());
+    m.digest().to_string()
+}
+
+fn compress(s: &String) -> Vec<u8> {
+    deflate::deflate_bytes_zlib(s.as_bytes()) 
+}
 
 fn main() {
     let content = "what is up, doc?\n";
     let header = format!("blob {}\0", content.len());
     let store = header + content;
-    let store_bytes = store.as_bytes();
-    let digest = digest::digest(&digest::SHA1, store_bytes);
-    let foo = digest.as_ref().to_hex();
-    let sha1 = foo.as_str();
-    println!("{}", sha1);
-    let mut encoder = ZlibEncoder::new(Vec::new(), Compression::Default);
-    encoder.write(store_bytes).unwrap();
-    let bytes = encoder.finish().unwrap();
-    println!("{}", bytes.to_hex());
 
-    let path = format!("{}/{}", ".git/objects", &sha1[0..2]);
+    let hash = sha1(&store);
+    println!("{:?}", hash);
+
+    let bytes = compress(&store);
+    println!("{:?}", bytes);
+
+    let path = format!("{}/{}", ".git/objects", &hash[0..2]);
     fs::create_dir_all(&path).unwrap();
 
-    let fname = format!("{}/{}", path, &sha1[2..40]);
+    let fname = format!("{}/{}", path, &hash[2..40]);
     let mut f = File::create(fname).unwrap();
     f.write_all(&bytes).unwrap();
 }
